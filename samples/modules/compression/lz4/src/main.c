3d107f5e82f541b59efb372b09ef81dfe06990f4
Hubert Miś <hubert.mis@nordicsemi.no>
2025-08-13 14:16:28 +0200

    samples: lz4: Measure duration of a firmware image decompression
    Port LZ4 sample to nRF54LM20 to measure performance (speed) of
    a firmware image decompression. This sample uses an ESB PRX image
    from another prestudy as the test data. The measurements are performed
    executing the code from RAM and RRAM. It is expected that RAM execution
    would be more efficient in real DFU scenario if it could be performed
    in parallel with RRAM writing of the previous block.
    
    The memory requirements of LZ4 render this algorithm unfitting nRF54L
    series. However its speed prooves that other algorithms like nrv2e or
    Exomizer with lower memory requirements and comparable speed could
    fit DFU well.
    
    Signed-off-by: Hubert Miś <hubert.mis@nordicsemi.no>

--- samples/modules/compression/lz4/src/main.c
+++ samples/modules/compression/lz4/src/main.c
@@ -5,77 +5,136 @@
  */
 
 #include <stdio.h>
+#include <stdint.h>
 #include <zephyr/kernel.h>
 #include <zephyr/misc/lorem_ipsum.h>
 #include <string.h>
 #include <stdlib.h>
+#include "lz4frame.h"
 #include "lz4.h"
+
+#include "lz4_array.h"
 
 int main(void)
 {
-	static const char *src = LOREM_IPSUM;
+    uint32_t decomp_start_time, decomp_end_time;
+    size_t src_size = 0;
+    static char decompressed_data[64 * 1024];
+    char *src = NULL;
 
-	const int src_size = (int)(strlen(src) + 1);
-	const int max_dst_size = LZ4_compressBound(src_size);
+    decomp_start_time = k_cycle_get_32();
 
-	char *compressed_data = malloc((size_t)max_dst_size);
+    const char *compressed_src = (const char *)zephyr_bin_lz4;
+    size_t src_size_remaining = sizeof(zephyr_bin_lz4) / sizeof(zephyr_bin_lz4[0]);
+    size_t src_offset = 0;
+    int decompressed_size = 0;
+    int total_decompressed_size = 0;
 
-	if (compressed_data == NULL) {
-		printk("Failed to allocate memory for compressed data\n");
-		return 0;
-	}
+    /* Allocate memory for decompressed data */
+    if (!decompressed_data) {
+     printf("Failed to allocate memory for decompressed data\n");
+     return 0;
+    }
 
-	const int compressed_data_size = LZ4_compress_default(src,
-					compressed_data, src_size,
-					max_dst_size);
-	if (compressed_data_size <= 0) {
-		printk("Failed to compress the data\n");
-		return 0;
-	}
+    /* Create LZ4F decompression context */
+    void *dctx = NULL;
+    size_t result = LZ4F_createDecompressionContext(&dctx, LZ4F_VERSION);
 
-	printk("Original Data size: %d\n", src_size);
-	printk("Compressed Data size : %d\n", compressed_data_size);
+    if (LZ4F_isError(result)) {
+     printf("Failed to create LZ4F decompression context: %s\n",
+      LZ4F_getErrorName(result));
+     return 0;
+    }
 
-	compressed_data = (char *)realloc(compressed_data,
-					  (size_t)compressed_data_size);
-	if (compressed_data == NULL) {
-		printk("Failed to re-alloc memory for compressed data\n");
-		return 0;
-	}
+    /* Get frame info */
+    LZ4F_frameInfo_t frameInfo;
+    size_t consumed = src_size_remaining;
+    result = LZ4F_getFrameInfo(dctx, &frameInfo, compressed_src, &consumed);
 
-	char * const decompressed_data = malloc(src_size);
+    if (LZ4F_isError(result)) {
+     printf("Failed to get LZ4 frame info: %s\n",
+      LZ4F_getErrorName(result));
+     LZ4F_freeDecompressionContext(dctx);
+     return 0;
+    }
 
-	if (decompressed_data == NULL) {
-		printk("Failed to allocate memory to decompress data\n");
-		return 0;
-	}
+    src_offset += consumed;
+    src_size_remaining -= consumed;
 
-	const int decompressed_size = LZ4_decompress_safe(compressed_data,
-				      decompressed_data, compressed_data_size,
-				      src_size);
-	if (decompressed_size < 0) {
-		printk("Failed to decompress the data\n");
-		return 0;
-	}
+    /* Decompress the data in streaming mode, overwriting the output buffer each time */
+    while (src_size_remaining > 0) {
+     /* Reset output buffer size for this iteration */
+     size_t dst_size = ARRAY_SIZE(decompressed_data);
+     size_t src_size_to_process = src_size_remaining;
 
-	free(compressed_data);
+     /* Decompress a chunk */
+     size_t ret = LZ4F_decompress(dctx,
+            decompressed_data,
+            &dst_size,
+            compressed_src + src_offset,
+            &src_size_to_process,
+            NULL);
 
-	if (decompressed_size >= 0) {
-		printk("Successfully decompressed some data\n");
-	}
+     if (LZ4F_isError(ret)) {
+      printf("Decompression failed: %s\n", LZ4F_getErrorName(ret));
+      LZ4F_freeDecompressionContext(dctx);
+      return 0;
+     }
 
-	if (decompressed_size != src_size) {
-		printk("Decompressed data is different from original\n");
-		return 0;
-	}
+     /* Update counters */
+     src_offset += src_size_to_process;
+     src_size_remaining -= src_size_to_process;
+     total_decompressed_size += dst_size;
 
-	if (memcmp(src, decompressed_data, src_size) != 0) {
-		printk("Validation failed.\n");
-		printk("*src and *new_src are not identical\n");
-		return 0;
-	}
+     /* If ret == 0, we've reached the end of the frame */
+     if (ret == 0) {
+      break;
+     }
+    }
 
-	printk("Validation done. The string we ended up with is:\n%s\n",
-			decompressed_data);
-	return 0;
+    /* Clean up */
+    LZ4F_freeDecompressionContext(dctx);
+
+    /* For comparison purposes with the original code, set decompressed_size
+       to the total size that has been decompressed */
+    decompressed_size = total_decompressed_size;
+    decomp_end_time = k_cycle_get_32(); /* Use k_cycle_get_32 instead of k_uptime_get_32 */
+
+    if (decompressed_size < 0) {
+     printf("Failed to decompress the data\n");
+     return 0;
+    }
+
+    /* Convert cycles to microseconds using CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC */
+    uint32_t decomp_time_us = (uint32_t)((decomp_end_time - decomp_start_time) *
+                             (1000000.0 / CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC));
+    printf("Decompression took %u us\n", decomp_time_us);
+
+    /* Remove free of compressed_data since it wasn't allocated in this code */
+
+    if (decompressed_size >= 0) {
+     printf("Successfully decompressed some data\n");
+     /* Hexdump the last 8 bytes of decompressed data */
+     printf("Last 8 bytes of decompressed data: ");
+     for (int i = 0; i < 8 && (decompressed_size % (64 * 1024) - i) > 0; i++) {
+         printf("%02x ", (unsigned char)decompressed_data[decompressed_size % (64 * 1024) - 8 + i]);
+     }
+     printf("\n");
+    }
+
+    if (decompressed_size != src_size) {
+     printf("Decompressed data is different from original: %zu != %zu\n", decompressed_size, src_size);
+     return 0;
+    }
+
+    if (memcmp(src, decompressed_data, src_size) != 0) {
+     printf("Validation failed.\n");
+     printf("*src and *new_src are not identical\n");
+     return 0;
+    }
+
+    printf("Validation done. The string we ended up with is:\n%s\n",
+      decompressed_data);
+
+    return 0;
 }
